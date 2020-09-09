5c39cd5e9d83c1094cce407a959e8366b0789da2
Aaro0n <mrcard97@gmail.com>
2020-09-09 10:39:51 +0800

    fix typos

--- src/Preview.vue
+++ src/Preview.vue
@@ -31,7 +31,7 @@ export default {
       gfm: true,
       tables: true, //支持Github形式的表格，必须打开gfm选项
       breaks: true, //支持Github换行符，必须打开gfm选项，填写true或者false
-      sanitize: false, //原始输出，忽略HTML标签，这个作为一个开发人员，一定要写flase
+      sanitize: false, //原始输出，忽略HTML标签，这个作为一个开发人员，一定要写false
       smartLists: true, //优化列表输出，这个填写ture之后，你的样式会好看很多，所以建议设置成ture
       smartypants: true,
       xhtml: false
